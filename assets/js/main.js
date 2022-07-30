453af7ec92d2afed7d268793329764f430a1826a
KazutoKashima <phoenixreid03@gmail.com>
2022-07-30 21:02:21 +1000

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,20 @@ const Home = {
     
     <main id='home'>
         <div class="about__me">
-            <img src="/img/avatar.svg" alt="user avatar">
+            <img class="avatar" src="/img/avatar-2-modified.svg" alt="user avatar"></img>
             <h1>Phoenix Reid</h1><br>
-            <h3>Full Stack Web Developer<br>Jnr. C++ Developer<br>Network Engineer</h3><br>
+            <h3>Full Stack Web Developer<br>Jnr. C++, JS, and Python Developer<br>Network Engineer</h3><br>
             <p>Hi there! 👋<br>I'm an, 18-year-old, aspiring-developer!<br>I've studied with RMIT (Melbourne/Australia) to gain the upper hand in understanding networking, followed by my current studies in Cyber Security with the University of Tasmania.
             <br><br>I'm currently looking for a job as a junior web developer, or a junior C++ developer, or a junior network engineer.
             <br><br>One of my most favourable projects, and most up to date, is <a href="https://scarletai.xyz" target="_blank">Scarlet</a>. An open source AI that uses very basic Natural Language Processing (NLP) and third-party anti-virus APIs to help protect users from potential phishing links, viruses and more!
-            <br><br>Another project that took a Red Team stand point is <a href="https://github.com/KazutoKashima/Project-Key-Breaker" target="_blank">Key Breaker</a>it can be used to retrieve, decipher, and exploit the weak security standards of Chrome's password manager.</p>
+            <br><br>Another project that took a Red Team stand point is <a href="https://github.com/KazutoKashima/Project-Key-Breaker" target="_blank">Key Breaker</a> it can be used to retrieve, decipher, and exploit the weak security standards of Chrome's password manager.</p>
             <br><br><div class="skills_projects_link">
                 <router-link to="/projects"> Projects | Skills </router-link>
             </div>
-            <footer>© Developed by <a href="https://github.com/KazutoKashima">Phoenix Reid</a>, with ❤️ and VueJS.</footer>
+            
+        <footer>
+            <p>© 2022 - Developed by <a href="https://github.com/KazutoKashima">Phoenix Reid</a>, with ❤️ and VueJS.</p>
+        </footer>
         </div>
     </main>
     
@@ -27,10 +30,10 @@ const Projects = {
             
         <header id="site_header" class="container d_flex">
             <div class="bio__media">
-                <img src="/img/avatar.svg" alt="user avatar">
+                <img src="/img/avatar-2-modified.svg" alt="user avatar">
                 <div class="bio__media__text">
                     <h1>Phoenix Reid</h1>
-                    <h3>Full Stack Web Developer<br>Jnr. C++ Developer<br>Network Engineer</h3>
+                    <h3>Full Stack Web Developer<br>Jnr. C++, JS, and Python Developer<br>Network Engineer</h3>
                     <p>Here's a <i>"short"</i> list of my GitHub repos!</p>
                 </div>
             </div> 
@@ -116,7 +119,7 @@ const Projects = {
         return {
             projects: [],
             projectsList: null,
-            skills: [],
+            skills: ["C#", "Python", "JavaScript", "C(++)"],
             projectsCount: 5,
             perPage: 20,
             loading : true,
@@ -126,7 +129,7 @@ const Projects = {
     },
     methods:{
         fetchData: function () {
-            axios.get(`https://api.github.com/users/KazutoKashima/repos?per_page=${this.perPage}`)
+            axios.get(`https://api.github.com/users/ScarletAIO/repos?per_page=${this.perPage}`)
                 .then(response => { 
                     console.log(response);
                     this.projects = response.data;
